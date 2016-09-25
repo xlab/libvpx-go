613a6d58c6f2431e6cc3528b33f9a1d93038eabb
Maxim Kupriianov <max@kc.vc>
2016-09-25 14:25:54 +0300

    Update manifest for libvpx-0.1.6.

--- vpx/iface.go
+++ vpx/iface.go
@@ -4,6 +4,8 @@ package vpx
 #cgo pkg-config: vpx
 #include <vpx/vp8dx.h>
 #include <vpx/vp8cx.h>
+#include <vpx/vp9dx.h>
+#include <vpx/vp9cx.h>
 #include <stdlib.h>
 */
 import "C"
@@ -12,9 +14,9 @@ func DecoderIfaceVP8() *CodecIface {
 	return (*CodecIface)(C.vpx_codec_vp8_dx())
 }
 
-// func DecoderIfaceVP9() *CodecIface {
-// 	return (*CodecIface)(C.vpx_codec_vp9_dx())
-// }
+func DecoderIfaceVP9() *CodecIface {
+	return (*CodecIface)(C.vpx_codec_vp9_dx())
+}
 
 // func DecoderIfaceVP10() *CodecIface {
 // 	return (*CodecIface)(C.vpx_codec_vp10_dx())
@@ -24,8 +26,8 @@ func DecoderFor(fourcc int) *CodecIface {
 	switch fourcc {
 	case Vp8Fourcc:
 		return DecoderIfaceVP8()
-		// case Vp9Fourcc:
-		// 	return DecoderIfaceVP9()
+	case Vp9Fourcc:
+		return DecoderIfaceVP9()
 	}
 	return nil
 }
@@ -34,9 +36,9 @@ func EncoderIfaceVP8() *CodecIface {
 	return (*CodecIface)(C.vpx_codec_vp8_cx())
 }
 
-// func EncoderIfaceVP9() *CodecIface {
-// 	return (*CodecIface)(C.vpx_codec_vp9_cx())
-// }
+func EncoderIfaceVP9() *CodecIface {
+	return (*CodecIface)(C.vpx_codec_vp9_cx())
+}
 
 // func EncoderIfaceVP10() *CodecIface {
 // 	return (*CodecIface)(C.vpx_codec_vp10_cx())
@@ -46,8 +48,8 @@ func EncoderFor(fourcc int) *CodecIface {
 	switch fourcc {
 	case Vp8Fourcc:
 		return EncoderIfaceVP8()
-		// case Vp9Fourcc:
-		// 	return EncoderIfaceVP9()
+	case Vp9Fourcc:
+		return EncoderIfaceVP9()
 	}
 	return nil
 }
