5cfe3b8966f4d8158b596793b4a5fa90a58f60ba
Maxim Kupriianov <max@kc.vc>
2016-09-25 18:05:33 +0300

    Add VP9.

--- vpx/iface.go
+++ vpx/iface.go
@@ -12,9 +12,9 @@ func DecoderIfaceVP8() *CodecIface {
 	return (*CodecIface)(C.vpx_codec_vp8_dx())
 }
 
-// func DecoderIfaceVP9() *CodecIface {
-// 	return (*CodecIface)(C.vpx_codec_vp9_dx())
-// }
+func DecoderIfaceVP9() *CodecIface {
+	return (*CodecIface)(C.vpx_codec_vp9_dx())
+}
 
 // func DecoderIfaceVP10() *CodecIface {
 // 	return (*CodecIface)(C.vpx_codec_vp10_dx())
@@ -24,8 +24,8 @@ func DecoderFor(fourcc int) *CodecIface {
 	switch fourcc {
 	case Vp8Fourcc:
 		return DecoderIfaceVP8()
-		// case Vp9Fourcc:
-		// 	return DecoderIfaceVP9()
+	case Vp9Fourcc:
+		return DecoderIfaceVP9()
 	}
 	return nil
 }
@@ -34,9 +34,9 @@ func EncoderIfaceVP8() *CodecIface {
 	return (*CodecIface)(C.vpx_codec_vp8_cx())
 }
 
-// func EncoderIfaceVP9() *CodecIface {
-// 	return (*CodecIface)(C.vpx_codec_vp9_cx())
-// }
+func EncoderIfaceVP9() *CodecIface {
+	return (*CodecIface)(C.vpx_codec_vp9_cx())
+}
 
 // func EncoderIfaceVP10() *CodecIface {
 // 	return (*CodecIface)(C.vpx_codec_vp10_cx())
@@ -46,8 +46,8 @@ func EncoderFor(fourcc int) *CodecIface {
 	switch fourcc {
 	case Vp8Fourcc:
 		return EncoderIfaceVP8()
-		// case Vp9Fourcc:
-		// 	return EncoderIfaceVP9()
+	case Vp9Fourcc:
+		return EncoderIfaceVP9()
 	}
 	return nil
 }
